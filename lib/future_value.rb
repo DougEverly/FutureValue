dc1e653dfde3fe5c38c7412d487b04b014a27c44
Douglas Everly <Doug@Everly.org>
2015-03-22 21:32:44 -0400

    Added initial value.

--- lib/future_value.rb
+++ lib/future_value.rb
@@ -2,25 +2,44 @@ require "future_value/version"
 require "thread"
 
 class FutureValue
-  def initialize
-    @value = nil
-    @mutex = Mutex.new
-    @condvar = ConditionVariable.new
-    @has_value = false
+  
+  # Initializes new FutureValue
+  # Pass value if known at instantiation
+  def initialize(*args)
+    if args.size == 1
+      @value = args.first
+      puts "Init value is #{@value}"
+      @has_value = true
+    else
+      @value = nil
+      @has_value = false
+      @mutex = Mutex.new
+      @condvar = ConditionVariable.new
+      @timeout = nil
+    end
+    
   end
   
+  # Returns true if FutureValue has a value
   def has_value?
       @has_value
   end
   
+  # Returns the value if has a value
+  # Blocks until a value it set
   def value
-    @mutex.synchronize {
-      return @value if @has_value
-      @condvar.wait(@mutex)
+    if @has_value
       return @value
-    }
+    else
+      @mutex.synchronize {
+        @condvar.wait(@mutex, @timeout)
+        return @value
+      }
+    end
   end
   
+  # Set a value
+  # Signal waiting threads that value is present
   def value=(value)
     @mutex.synchronize {
       if has_value?
